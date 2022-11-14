6b348348f4081a2882e8743789e110a17ca7ba5a
dcalligy <dcalligy813@gmail.com>
2022-11-13 19:25:40 -0500

    Adding some TODOs. Created a function that will check if the record being sent from the user already exist. If it is true we update that record, and insert the rest.

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,27 @@ app.use(function(req, res, next) {
   next();
 });
 
+// TODO: We should add all of the fields to the return object.
+// That way we can update the record with the correct data.
+// We also need to edit our query to update the record appropriately.
+// I currently don't think that our query is the correct approach to update it.
+async function checkExist(client, body) {
+  let return_obj = {
+    found: false,
+  };
+  const sql = await client.query(
+    `SELECT * FROM items_inventory WHERE description = $1`
+  ,[body.description]);
+  if (sql && sql.rows.length == 1) {
+    console.log('are we not here?');
+    return_obj = {
+      item_id: sql.rows[0].items_id,
+      found: true,
+    }
+  }
+  return return_obj;
+}
+
 // Set up a route home.
 app.get('/', pgMiddleware(async (client, req, res, next) => {
   try {
@@ -35,16 +56,30 @@ app.post('/add', pgMiddleware(async (client, req, res, next) => {
   // adding the same item multiple times.
   try {
     await client.query('BEGIN;');
-    if (req.body) {
+    const check_exist = await checkExist(client, req.body);
+    if (check_exist.found) {
+      console.log('found');
+      await client.query(`
+        UPDATE items_inventory
+        SET description = $1, qty_on_hand = $2, qty_desired = $3, qty_needed = $4
+        WHERE items_id = $5
+      `[req.body.description,
+        req.body.qty_on_hand,
+        req.body.qty_desired,
+        req.body.qty_need,
+        check_exist.item_id
+      ]);
+    } else {
+      console.log('we are in the else block');
       await client.query(`
         INSERT INTO items_inventory
           (items_id, description, qty_on_hand, qty_desired, qty_needed)
-         VALUES
-         (nextval('items_inventory_items_id_seq'), $1, $2, $3, $4)
+        VALUES
+        (nextval('items_inventory_items_id_seq'), $1, $2, $3, $4)
       `, [req.body.description, req.body.qty_on_hand, req.body.qty_desired, req.body.qty_needed]);
-      await client.query('COMMIT;');
-      res.status(200).send('OK');
     }
+    await client.query('COMMIT;');
+    res.status(200).send('OK');
   } catch (err) {
     await client.query('ROLLBACK;');
     res.status(500).send(err);
